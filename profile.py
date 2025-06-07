446c76090302563212f5c555a6ac98ad97e84ff0
dannydanners <144188071+dannydanners@users.noreply.github.com>
2025-06-07 02:27:07 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -10,13 +10,13 @@ request = portal.context.makeRequestRSpec()
 
 node_webserver = request.XenVM("webserver")
 node_webserver.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
-node_webserver.routable_control_ip = "true"
+node_webserver.routable_control_ip = True
 node_webserver.addService(rspec.Execute(shell="/bin/bash", command="sudo apt-get update && sudo apt-get install -y apache2 && sudo systemctl start apache2 && sudo apt -y install nfs-common"))
 iface0 = node_webserver.addInterface('interface-1', rspec.IPv4Address('10.1.1.2','255.255.255.0'))
 
 node_observer = request.XenVM("observer")
 node_observer.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
-node_observer.routable_control_ip = "false"
+node_observer.routable_control_ip = False
 node_observer.addService(rspec.Execute(shell="/bin/bash", command="sudo apt-get update && sudo apt -y install nfs-kernel-server"))
 iface1 = node_observer.addInterface('interface-0', rspec.IPv4Address('10.1.1.1','255.255.255.0'))
 

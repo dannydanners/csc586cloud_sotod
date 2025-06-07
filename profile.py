f65a1f8fec161a3195371a92dc7e78fcdff3ab91
dannydanners <144188071+dannydanners@users.noreply.github.com>
2025-06-06 21:47:00 -0400

    added new nodes

--- profile.py
+++ profile.py
@@ -11,12 +11,12 @@ node.routable_control_ip = "true"
 node2 = request.XenVM("webserver")
 node2.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
 node2.routable_control_ip = "true"
-node2.addService(pg.Execute(shell="/bin/bash", command="sudo apt-get update && sudo apt-get install -y apache2 && sudo systemctl start apache2 && sudo apt -y install nfs-common"))
+node2.addService(rspec.Execute(shell="/bin/bash", command="sudo apt-get update && sudo apt-get install -y apache2 && sudo systemctl start apache2 && sudo apt -y install nfs-common"))
 
 node3 = request.XenVM("observer")
 node3.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
 node3.routable_control_ip = "false"
-node3.addService(pg.Execute(shell="/bin/bash", command="sudo apt-get update && sudo apt -y install nfs-kernel-server"))
+node3.addService(rspec.Execute(shell="/bin/bash", command="sudo apt-get update && sudo apt -y install nfs-kernel-server"))
 
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()

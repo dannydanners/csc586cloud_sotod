d9cc46d5c5edac432686e67cf9ada84315789fab
Daniel Soto <dan.soto14@gmail.com>
2025-06-22 17:10:46 -0400

    fixed formatting

--- ldap-profile.py
+++ ldap-profile.py
@@ -16,15 +16,15 @@ for i in range(3):
     else:
         node = request.XenVM("ldap")   
 
-node.routable_control_ip = "true"
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
-iface = node.addInterface("if" + str(i))
-iface.component_id = "eth1"
-iface.addAddress(rspec.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
-link.addInterface(iface)
+    node.routable_control_ip = "true"
+    node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
+    iface = node.addInterface("if" + str(i))
+    iface.component_id = "eth1"
+    iface.addAddress(rspec.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+    link.addInterface(iface)
   
-if i == 0:
-    node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/setup_apache.sh"))
+    if i == 0:
+        node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/setup_apache.sh"))
     
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
